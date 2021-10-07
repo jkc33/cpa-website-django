d4156ab5edea15169d932a53a19e753ff5b64147
Ubuntu <ubuntu@ip-172-31-18-189.us-west-1.compute.internal>
2021-10-07 21:37:58 +0000

    favicon

--- main/urls.py
+++ main/urls.py
@@ -1,7 +1,9 @@
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
 from . import views
+from django.views.generic.base import RedirectView
+from django.contrib.staticfiles.storage import staticfiles_storage
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -11,4 +13,8 @@ urlpatterns = [
     path('category/<int:id>', views.category, name='category'),
     path('search-results', views.search_results, name='search-results'),
     path('about', views.about, name='about'),
+    path(
+        "favicon.ico",
+        RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")),
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
